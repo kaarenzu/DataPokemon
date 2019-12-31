0f7c1d9fc1d6e562a5c0b356d5586dad41017c12
kaarenzu <karenzcortes0211@gmail.com>
2019-12-31 12:44:03 -0300

    Agregue los personajes en pantalla principal y cree la tarjeta para jugadores

--- src/main.js
+++ src/main.js
@@ -11,28 +11,65 @@ student(POTTER); staff(POTTER); unicorn(POTTER); dragon(POTTER);
 phoenix(POTTER); pureBlood(POTTER); halfblood(POTTER); muggle(POTTER);
 
 // Tarjeta de jugadores
-
+function crearCard(POTTER){
+  for(let i = 0; POTTER.length; i ++){
+    const container = document.getElementById('container');
+       container.innerHTML += `<div class="wrap">
+      <div class="tarjeta-wrap">
+        <div class="tarjeta">
+          <div class="adelante card1">
+          <img src =  ${POTTER[i].image} class= "imagenes">
+          <li class="descripcion" >Name: ${(POTTER)[i].name}
+          <li class= "descripcion">Specie: ${(POTTER)[i].species}
+          <li class= "descripcion">House: ${(POTTER)[i].house}
+          <li class= "descripcion">Core: ${(POTTER)[i].wand.core}
+          <li class= "descripcion">Patronus: ${(POTTER)[i].patronus} </div>
+          <div class="atras">
+          <li class="descripcion" >Gender: ${(POTTER)[i].gender}
+          <li class= "descripcion">DateOfBirth: ${(POTTER)[i].dateOfBirth}
+          <li class= "descripcion">YearOfBirth: ${(POTTER)[i].yearOfBirth}
+          <li class= "descripcion">Ancestry: ${(POTTER)[i].ancestry}
+          <li class= "descripcion">Actor: ${(POTTER)[i].actor}</div>
+          </div>
+        </div>
+      </div>`; 
+    }
+}
+// Mostrar todos los jugadores en pantalla
+for(let i = 0; POTTER.length; i ++){
   const container = document.getElementById('container');
-  const tarjetaJugadores= `<div class="wrap">
-  <div class="tarjeta-wrap">
-    <div class="tarjeta">
-      <div class="adelante card1"><img src = class= "imagenes">
-      <li class="descripcion" >Name:
-      <li class= "descripcion">Specie:
-      <li class= "descripcion">House: 
-      <li class= "descripcion">Core: 
-      <li class= "descripcion">Patronus: </div>
-      <div class="atras">
-      <li class="descripcion" >Gender:
-      <li class= "descripcion">DateOfBirth: 
-      <li class= "descripcion">YearOfBirth: 
-      <li class= "descripcion">Ancestry: 
-      <li class= "descripcion">Actor: </div> 
+     container.innerHTML += `<div class="wrap">
+    <div class="tarjeta-wrap">
+      <div class="tarjeta">
+        <div class="adelante card1">
+        <img src =  ${POTTER[i].image} class= "imagenes">
+        <li class="descripcion" >Name: ${(POTTER)[i].name}
+        <li class= "descripcion">Specie: ${(POTTER)[i].species}
+        <li class= "descripcion">House: ${(POTTER)[i].house}
+        <li class= "descripcion">Core: ${(POTTER)[i].wand.core}
+        <li class= "descripcion">Patronus: ${(POTTER)[i].patronus} </div>
+        <div class="atras">
+        <li class="descripcion" >Gender: ${(POTTER)[i].gender}
+        <li class= "descripcion">DateOfBirth: ${(POTTER)[i].dateOfBirth}
+        <li class= "descripcion">YearOfBirth: ${(POTTER)[i].yearOfBirth}
+        <li class= "descripcion">Ancestry: ${(POTTER)[i].ancestry}
+        <li class= "descripcion">Actor: ${(POTTER)[i].actor}</div>
+        </div>
       </div>
-    </div>
-  </div>`;
-  
-// Hacer que filtre por gryffindor 
+    </div>`; 
+  }
 
-const botonGryffindor = document.getElementById('gryffindor');
-botonGryffindor.addEventListener( 'click', tarjetaGryffindor );
+  // Mostrar filtrado gryffindor
+
+  const btnGryffindor = document.getElementById("gryffindor");
+  btnGryffindor.addEventListener('click', () => {
+    for(let i = 0; POTTER.length; i++) {
+      container.innerHTML = ' ';
+    const filtradoGryffindor = gryffindor(data);
+    crearCard(filtradoGryffindor);
+    container.innerHTML = crearCard(filtradoGryffindor);
+
+    }
+    
+   
+  });
